9e76ab50721b673e501447eec3d412791c5e5697
AzizjonShamsiyev <azizjon5629@gmail.com>
2024-05-05 13:15:09 +0500

    Basic/2-amaliy

---
+++ Calc/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("1-son : ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Operator(+,-,*,/) : ");
+var op = Console.ReadLine();
+Console.Write("2-son : ");
+double b =  double.Parse(Console.ReadLine());
+
+switch(op)
+{
+    case "+":
+        Console.WriteLine($"c = {a+b}");
+        break;
+
+    case "-":
+        Console.WriteLine($"c = {a - b}");
+        break;
+
+    case "*":
+        Console.WriteLine($"c = {a * b}");
+        break;
+
+    case "/":
+        Console.WriteLine($"c = {a / b}");
+        break;
+    default:
+        Console.WriteLine("Operator xato!");
+        break;
+}
---
+++ Raqamlar qatorining yig‘indisini hisoblash/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+for (int i = 1; i <= n; i++)
+    summ += i;
+Console.WriteLine(summ);

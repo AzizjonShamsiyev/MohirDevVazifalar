d36e122971f4f39c96adeec7a964491860517a1e
AzizjonShamsiyev <azizjon5629@gmail.com>
2024-05-05 12:56:38 +0500

    Basic/1-amaliy vazifa

---
+++ ValyutaKonvertri/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Qiymat : ");
+double q = double.Parse(Console.ReadLine());
+Console.Write("Kurs : ");
+double k = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"{q*k} sum");
---
+++ Vazifa1/Program.cs
@@ -0,0 +1,7 @@
+﻿double r, s, l;
+Console.Write("R = ");
+r = double.Parse(Console.ReadLine());
+s = Math.PI * Math.Pow(r, 2);
+l = 2 * Math.PI * r;
+Console.WriteLine($"S = {s}");
+Console.WriteLine($"L = {l}");

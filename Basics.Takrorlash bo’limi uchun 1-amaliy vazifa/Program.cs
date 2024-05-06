75ff33733ec75996ec2aa2aafddcd63fbc502e02
AzizjonShamsiyev <azizjon5629@gmail.com>
2024-05-06 19:25:00 +0500

    UPLOAD: Takrorlash bo’limi uchun 1-amaliy vazifa

--- Basics.Takrorlash bo’limi uchun 1-amaliy vazifa/Program.cs
+++ Basics.Takrorlash bo’limi uchun 1-amaliy vazifa/Program.cs
@@ -1,1 +1,47 @@
 ﻿
+static void Task1()
+{
+    int i = 1;
+    int sum = 0;
+    while (i <= 1000)
+    {
+        if (i % 2 != 0)
+        {
+            sum += i;
+        }
+        i++;
+    }
+    Console.WriteLine(sum);
+}
+
+static void Task2()
+{ 
+    int[] array = { 2,3,-24,435,63,-32,1,22 };
+    int min = array[0];
+    int max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    Console.WriteLine(min * max);
+}
+
+static void Task3()
+{
+    Console.Write("Kiriting : ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    long factorial = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        factorial *= i;
+    }
+    Console.WriteLine(x + " => " + factorial);
+
+}
